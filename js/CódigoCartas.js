e3cce829c897284ec3963534c9f54f7100d0b29a
Gabriel Bezerra de Jesus <114784090+SwattPK@users.noreply.github.com>
2022-11-21 22:18:16 -0400

    update

--- js/CódigoCartas.js
+++ js/CódigoCartas.js
@@ -2,19 +2,15 @@ let carta = document.querySelectorAll('.carta')
 let inicial = document.getElementById('telaInicial')
 let costa = document.querySelectorAll('.costa')
 let principal = document.getElementById('mexe')
+let placarJogador;
 
 
-
-function player() {
-    let nomeJogador = JSON.parse(localStorage.getItem('nomeJogador'));
-    document.getElementById('player').innerHTML = `${nomeJogador} `
-}
-player()
-
+let mexe = document.getElementById('mexe')
 let cartas = [carta[0],carta[1],carta[2],carta[3],carta[4]];
 let costas = [costa[0],costa[1],costa[2],costa[3],costa[4]];
 function start() {
     document.getElementById('numero').innerHTML = cartas.length;
+    placarJogador = document.getElementById('placarJogador')
     let aleatorio = Math.floor(Math.random()*cartas.length);
     let resultado = cartas[aleatorio];
     let mostra = [inicial.style.opacity = '0', inicial.style.transition = '1s', 
@@ -24,12 +20,20 @@ function start() {
          mostra[i];
     }
 
+    setTimeout(() =>{
+        mexe.style.display = 'block';
+        placarJogador.style.display = 'flex';
+    }, 1000)
+
     setTimeout(function () {
         inicial.style.display ='none';
         principal.style.display ='block';
         principal.style.transition = '2.5s';
+        placarJogador.style.transition = '3s';
+        placarJogador.style.transform = 'translateX(0px)';
         principal.style.transform = 'translateY(0px)';
         resultado.style.transform = 'translateY(0px)';
+
     }, 1200)
 }
 
@@ -40,6 +44,24 @@ function virar () {
         costas[i].style.transform = "rotateY(180deg)";
     }
 }
+
+let nomeJogador;
+let soma = 0;
+function player() {
+    nomeJogador = JSON.parse(localStorage.getItem('nomeJogador'));
+    for (let i = 0; i < nomeJogador.length; i++) {
+        soma++
+        let nomesAbaixo = document.getElementById('nomesAbaixo');
+        let paragrafo = document.createElement('p');
+        nomesAbaixo.appendChild(paragrafo);
+        paragrafo.setAttribute('id',`paragrafo${i}`);
+        let escreverParagrado = document.getElementById(`paragrafo${i}`);
+        escreverParagrado.innerHTML = `Jogador º${soma}: ${nomeJogador[i]}`;
+
+    }
+}
+player()
+
 
 
 
--- js/códigoMenu.js
+++ js/códigoMenu.js
@@ -1,7 +1,6 @@
 let tagP = document.querySelectorAll('.acessar');
 
 tagP.forEach(tagPs => {tagPs.addEventListener('click', () =>{
-        console.log(tagPs);
         document.getElementById('menu1').setAttribute('style','display:none');
         setTimeout(() => {
         let menus = document.getElementById(`game-${tagPs.id}`)
@@ -15,10 +14,33 @@ tagP.forEach(tagPs => {tagPs.addEventListener('click', () =>{
 
 function jogar() {
     let jogadores = []
-    let jogador = document.getElementById('jogador')
-    jogadores.push(jogador.value);
-    console.log(jogadores);
+    let jogador = document.querySelectorAll('.jogador')
+    for (valores of jogador) {
+      jogadores.push(valores.value)
+    }
     let transformando = JSON.stringify(jogadores)
     localStorage.setItem('nomeJogador', transformando);
     window.location ='htmlDasCartas.html';
+    console.log(jogadores);
+}
+let legenda = document.getElementById('legenda')
+
+document.getElementById('adicionar').addEventListener('mouseover', () => {
+    legenda.removeAttribute('style')
+    legenda.setAttribute('style','display:relative; font-size:10px;')
+
+})
+
+let adicionar = document.getElementById('adicionar')
+
+adicionar.addEventListener('mouseout', () => {
+   setTimeout(() => { legenda.setAttribute('style','display:none;')}, 1000)
+})
+let novosInputs = document.getElementById('novosInputs');
+function adicionarInput() {
+    let criarInput = document.createElement('input');
+    criarInput.classList.add('jogador')
+    criarInput.setAttribute('type','text')
+    criarInput.setAttribute('placeholder','nome do jogador')
+    novosInputs.appendChild(criarInput);
 }
